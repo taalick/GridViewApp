338a724fffbf0545b07c640731eb7ae1c9fb3406
taalick <ta_alick@mail.ru>
2015-12-01 23:18:59 +0200

    commit first

---
+++ src/com/example/gridviewapp/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.gridviewapp;
+
+import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.Toast;
+import android.view.*;
+
+public class MainActivity extends Activity {
+
+	String[] countries = { "Brasilia", "Аргентина", "Чили", "Колумбия",
+			"Уругвай", "Парагвай" };
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		GridView countriesList = (GridView) findViewById(R.id.gridview);
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, countries);
+		countriesList.setAdapter(adapter);
+
+		OnItemClickListener itemListener = new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> parent, View v,
+					int position, long id) {
+				Toast.makeText(
+						getApplicationContext(),
+						"Вы выбрали "
+								+ parent.getItemAtPosition(position).toString(),
+						Toast.LENGTH_SHORT).show();
+			}
+		};
+		countriesList.setOnItemClickListener(itemListener);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
